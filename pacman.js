bb88765a07cc8ea42cd9ebf56645a505968bc85b
Binwei Xu <binwei.xu@appfolio.com>
2020-03-05 16:00:24 -0800

    te/js/bx - replace ghosts with images

--- pacman.js
+++ pacman.js
@@ -26,10 +26,12 @@ Pacman.FPS = 30;
 Pacman.Ghost = function (game, map, colour) {
 
     var position  = null,
+        prevPosition = null,
         direction = null,
         eatable   = null,
         eaten     = null,
-        due       = null;
+        due       = null,
+        ghostImage= null;
 
     function getNewCoord(dir, current) {
         var speed  = isVunerable() ? 1 : isHidden() ? 4 : 2,
@@ -41,6 +43,11 @@ Pacman.Ghost = function (game, map, colour) {
             "y": addBounded(current.y, ySpeed)
         };
     };
+
+    function setGhost(ghostname) {
+      ghostImage = new Image();
+      ghostImage.src = 'img/corgi.png';
+    }
 
     /* Collision detection(walls) is done when a ghost lands on an
      * exact block, make sure they dont skip over it
@@ -78,8 +85,10 @@ Pacman.Ghost = function (game, map, colour) {
         eaten = null;
         eatable = null;
         position = {"x": 90, "y": 80};
+        prevPosition = {"x": 90, "y": 80};
         direction = getRandomDirection();
         due = getRandomDirection();
+        setGhost();
     };
 
     function onWholeSquare(x) {
@@ -152,54 +161,10 @@ Pacman.Ghost = function (game, map, colour) {
             eaten = null;
         }
 
-        var tl = left + s;
-        var base = top + s - 3;
-        var inc = s / 10;
+        ctx.drawImage(ghostImage, left, top, 18, 18);
 
-        var high = game.getTick() % 10 > 5 ? 3  : -3;
-        var low  = game.getTick() % 10 > 5 ? -3 : 3;
-
-        ctx.fillStyle = getColour();
-        ctx.beginPath();
-
-        ctx.moveTo(left, base);
-
-        ctx.quadraticCurveTo(left, top, left + (s/2),  top);
-        ctx.quadraticCurveTo(left + s, top, left+s,  base);
-
-        // Wavy things at the bottom
-        ctx.quadraticCurveTo(tl-(inc*1), base+high, tl - (inc * 2),  base);
-        ctx.quadraticCurveTo(tl-(inc*3), base+low, tl - (inc * 4),  base);
-        ctx.quadraticCurveTo(tl-(inc*5), base+high, tl - (inc * 6),  base);
-        ctx.quadraticCurveTo(tl-(inc*7), base+low, tl - (inc * 8),  base);
-        ctx.quadraticCurveTo(tl-(inc*9), base+high, tl - (inc * 10), base);
-
-        ctx.closePath();
-        ctx.fill();
-
-        ctx.beginPath();
-        ctx.fillStyle = "#FFF";
-        ctx.arc(left + 6,top + 6, s / 6, 0, 300, false);
-        ctx.arc((left + s) - 6,top + 6, s / 6, 0, 300, false);
-        ctx.closePath();
-        ctx.fill();
-
-        var f = s / 12;
-        var off = {};
-        off[RIGHT] = [f, 0];
-        off[LEFT]  = [-f, 0];
-        off[UP]    = [0, -f];
-        off[DOWN]  = [0, f];
-
-        ctx.beginPath();
-        ctx.fillStyle = "#000";
-        ctx.arc(left+6+off[direction][0], top+6+off[direction][1],
-                s / 15, 0, 300, false);
-        ctx.arc((left+s)-6+off[direction][0], top+6+off[direction][1],
-                s / 15, 0, 300, false);
-        ctx.closePath();
-        ctx.fill();
-
+        prevPosition.x = position.x;
+        prevPosition.y = position.y;
     };
 
     function pane(pos) {
